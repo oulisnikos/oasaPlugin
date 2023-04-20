49f5fcf9c8b7cc3fda286780b977db109f811dfd
Oulis Nikos <oulisnikos@gmail.com>
2023-04-20 12:52:03 +0300

    Changes

--- oasa-sync-model/Line.go
+++ oasa-sync-model/Line.go
@@ -1,24 +1,24 @@
 package oasa_sync_model
 
-//type Line struct {
-//	ID       int64   `json:"LineCode" gorm:"primaryKey"`
-//	CODE     string  `json:"LineID"`
-//	DESCR    string  `json:"LineDescr"`
-//	DESCRENG string  `json:"LineDescrEng"`
-//	NUM1     int     `json:"Num1"`
-//	NUM2     float32 `json:"Num2"`
-//	NUM3     int     `json:"Num3"`
-//	NUM4     float32 `json:"Num4"`
-//	NUM5     int     `json:"Num5"`
-//	NUM6     float32 `json:"Num6"`
-//	NUM7     int     `json:"Num7"`
-//	NUM8     float32 `json:"Num8"`
-//	NUM9     int     `json:"Num9"`
-//	NUM10    float32 `json:"Num10"`
-//	NUM11    int     `json:"Num11"`
-//	NUM12    float32 `json:"Num12"`
-//	NUM13    int     `json:"Num13"`
-//	NUM14    float32 `json:"Num14"`
-//}
-//
-//type LineList []Line
+type Line struct {
+	ID       int64   `json:"LineCode" gorm:"primaryKey"`
+	CODE     string  `json:"LineID"`
+	DESCR    string  `json:"LineDescr"`
+	DESCRENG string  `json:"LineDescrEng"`
+	NUM1     int     `json:"Num1"`
+	NUM2     float32 `json:"Num2"`
+	NUM3     int     `json:"Num3"`
+	NUM4     float32 `json:"Num4"`
+	NUM5     int     `json:"Num5"`
+	NUM6     float32 `json:"Num6"`
+	NUM7     int     `json:"Num7"`
+	NUM8     float32 `json:"Num8"`
+	NUM9     int     `json:"Num9"`
+	NUM10    float32 `json:"Num10"`
+	NUM11    int     `json:"Num11"`
+	NUM12    float32 `json:"Num12"`
+	NUM13    int     `json:"Num13"`
+	NUM14    float32 `json:"Num14"`
+}
+
+type LineList []Line
